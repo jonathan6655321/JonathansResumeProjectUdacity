8f30b59f879aca21b66a0fe3ce0c929a2982bee0
Jonathan Somer <somer@mail.tau.ac.il>
2016-08-19 18:33:52 +0300

    finalllllll

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -20,21 +20,19 @@ var bio = {
             var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
             var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
             var contactsList = [formattedMobile, formattedEmail, formattedGithub];
-            for (var contactIndex in contactsList) {
-                $("#topContacts").append(contactsList[contactIndex]);
-            }
+            contactsList.forEach(function(val, contactIndex) {
+                $("#topContacts, #footerContacts ").append(contactsList[contactIndex]);
+            });
 
             var formattedbioPic = HTMLbioPic.replace("%data%", bio.biopic);
-            $("#header").append(formattedbioPic);
-
             var formattedWelcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
-            $("#header").append(formattedWelcomeMessage);
+            $("#header").append(formattedbioPic, formattedWelcomeMessage);
 
             if (bio.skills.length > 0) {
                 $("#header").append(HTMLskillsStart);
-                bio.skills.forEach(function(skill){
-                  $("#skills").append(HTMLskills.replace("%data%", skill));
-                })
+                bio.skills.forEach(function(skill) {
+                    $("#skills").append(HTMLskills.replace("%data%", skill));
+                });
             }
         } // described later.
 };
@@ -57,23 +55,24 @@ var education = {
         "url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
     }],
     "display": function() {
-        for (var i in education.schools) {
+        education.schools.forEach(function(val, i) {
             var educationStart = HTMLschoolStart;
             $("#education").append(educationStart);
             var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[i].name);
             var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[i].degree);
             $(".education-entry:last").append(formattedSchoolName + formattedDegree);
             var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[i].dates);
-            $(".education-entry:last").append(formattedSchoolDates);
-            for (var j in education.schools[i].majors) {
+            var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[i].location);
+            $(".education-entry:last").append(formattedSchoolDates, formattedSchoolLocation);
+            education.schools[i].majors.forEach(function(val, j) {
                 var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[i].majors[j]);
                 $(".education-entry:last").append(formattedMajor);
-            }
-        }
+            });
+        });
         if (education.onlineCourses.length > 0) {
             var onlineCoursesStart = HTMLonlineClasses;
             $("#education").append(onlineCoursesStart);
-            for (i in education.onlineCourses) {
+            education.onlineCourses.forEach(function(val, i) {
                 var onlineStart = HTMLschoolStart;
                 $("#education").append(onlineStart);
                 var onlineCoursesTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[i].title);
@@ -81,7 +80,7 @@ var education = {
                 $(".education-entry:last").append(onlineCoursesTitle + onlineCoursesSchool);
                 var onlineCoursesDates = HTMLonlineDates.replace("%data%", education.onlineCourses[i].dates);
                 $(".education-entry:last").append(onlineCoursesDates);
-            }
+            });
         }
     }
 };
@@ -103,16 +102,17 @@ var work = {
         "description": "In the wild, males seldom live longer than 10 to 14 years, as injuries sustained from continual fighting with rival males greatly reduce their longevity.[6] In captivity they can live more than 20 years. They typically inhabit savanna and grassland, although they may take to bush and forest. Lions are unusually social compared to other cats. A pride of lions consists of related females and offspring and a small number of adult males. Groups of female lions typically hunt together, preying mostly on large ungulates. Lions are apex and keystone predators, although they are also expert scavengers obtaining over 50 percent of their food by scavenging as opportunity allows. While lions do not typically hunt humans, some have. Sleeping mainly during the day, lions are active primarily at night (nocturnal), although sometimes at twilight (crepuscular).[7][8]"
     }],
     "display": function() {
-      work.jobs.forEach(function(val, jobIndex){
-        $("#workExperience").append(HTMLworkStart);
-        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[jobIndex].employer);
-        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[jobIndex].title);
-        $(".work-entry:last").append(formattedEmployer + formattedTitle);
-        var formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs[jobIndex].dates);
-        $(".work-entry:last").append(formattedWorkDates);
-        var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[jobIndex].description);
-        $(".work-entry:last").append(formattedWorkDescription);
-      });
+        work.jobs.forEach(function(val, jobIndex) {
+            $("#workExperience").append(HTMLworkStart);
+            var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[jobIndex].employer);
+            var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[jobIndex].title);
+            $(".work-entry:last").append(formattedEmployer + formattedTitle);
+            var formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs[jobIndex].dates);
+            var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[jobIndex].location);
+            $(".work-entry:last").append(formattedWorkDates, formattedWorkLocation);
+            var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[jobIndex].description);
+            $(".work-entry:last").append(formattedWorkDescription);
+        });
     }
 };
 
@@ -131,15 +131,15 @@ var projects = {
         "images": ["https://unsplash.it/300/300/?random", "https://unsplash.it/300/300/?random", "https://unsplash.it/300/300/?random"]
     }],
     "display": function() {
-        for (var projectIndex in projects.projects) {
+        projects.projects.forEach(function(val, projectIndex) {
             $("#projects").append(HTMLprojectStart);
             $(".project-entry:last").append(HTMLprojectTitle.replace("%data%", projects.projects[projectIndex].title));
             $(".project-entry:last").append(HTMLprojectDates.replace("%data%", projects.projects[projectIndex].dates));
             $(".project-entry:last").append(HTMLprojectDescription.replace("%data%", projects.projects[projectIndex].description));
-            for (var imageIndex in projects.projects[projectIndex].images) {
+            projects.projects[projectIndex].images.forEach(function(val, imageIndex) {
                 $(".project-entry:last").append(HTMLprojectImage.replace("%data%", projects.projects[projectIndex].images[imageIndex]));
-            }
-        }
+            });
+        });
     }
 };
 
